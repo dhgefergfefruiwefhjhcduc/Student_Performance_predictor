0916f9a2b0304b805152b67ba66c25ddacfea951
Priyanshu Mathur <mathurpriyanshu2006@gmail.com>
2025-07-20 20:00:41 +0530

    sixth

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 from dataclasses import dataclass
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 class DataIngestionConfig:
     train_data_path=os.path.join("artifacts","train_data.csv")
     test_data_path=os.path.join("artifacts","test_data.csv")
@@ -44,5 +47,8 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
     
